30275adae422a9949743a561ba73e0b9502f5462
Christopher Harlow <charlow@biltmore.com>
2022-07-27 16:07:08 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
+//MUST HAVE THE FOLLOWING LINE IN ORDER TO USE useState AND useEffect
+import {useState, useEffect} from 'react';
+
+//IMPORTING THE STYLES FROM THE APP.CSS FILE
 import './App.css';
+import SearchIcon from './search.svg';
+
+//IMPORTING IN COMPONENTS
+import MovieCard from './MovieCard';
+
+const API_URL = 'http://www.omdbapi.com?apikey=790bff7a';
+
+const movie1 = {
+  Poster: "https://m.media-amazon.com/images/M/MV5BYzE5MjY1ZDgtMTkyNC00MTMyLThhMjAtZGI5OTE1NzFlZGJjXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
+  Title: "Deadpool",
+  Type: "movie",
+  Year: "2016",
+  imdbID: "tt1431045"
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  //SETS UP THE ASYNCHRONOUS FETCHING OF DATA FROM THE API
+  const searchMovies = async (title) => {
+    const responses = await fetch(`${API_URL}&s=${title}`);
+    const data = await responses.json();
+
+    setMovies(data.Search);
+  };
+
+  //useEffect CAUSES THE INCLUDED FUNCTION TO TRIGGER IMMEDIATELY
+  useEffect(() => {
+    searchMovies('deadpool');
+  }, []);
+
+  return(
+    <div className="app">
+      <h1>MovieLand</h1>
+
+      <div className="search">
+        <input
+        type="text"
+        placeholder='Search for movies'
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)} />
+
+        <img
+        src={SearchIcon}
+        alt="search"
+        onClick={() => searchMovies(searchTerm)}/>
+      </div>
+
+      {
+        movies?.length > 0
+          ? (
+            <div className="container">
+              {movies.map((movie) => {
+                return <MovieCard movie={movie}/>
+              })}
+            </div>
+          ) : (
+            <div className="empty">
+              <h2>No movies found</h2>
+            </div>
+          )
+      }
+
+      
     </div>
   );
 }
---
+++ src/MovieCard.js
@@ -0,0 +1,23 @@
+function MovieCard({movie}){
+    console.log(movie.Poster);
+    console.log('hello');
+
+    return(
+        <div className="movie">
+            <div>
+            <p>{movie.Year}</p>
+            </div>
+
+            <div>
+            <img src={movie.Poster != "N/A" ? movie.Poster : 'https://via.placeholder.com/400'} />
+            </div>
+
+            <div>
+            <span>{movie.Type}</span>
+            <h3>{movie.Title}</h3>
+            </div>
+        </div>
+    )
+}
+
+export default MovieCard;
